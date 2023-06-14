f843a918817a1c8b347d25adae31d55efb71c266
jdnoguera <josias17.noguera@gmail.com>
2023-06-14 17:48:44 -0400

    tasks completed

---
+++ DefontanaTest/Infrastructure/Entities/Marca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefontanaTest.Infrastructure.Entities;
+
+public partial class Marca
+{
+    public long IdMarca { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public virtual ICollection<Producto> Productos { get; set; } = new List<Producto>();
+}
